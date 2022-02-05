a161a57ba5627a1ec3fa4aba99d71ed439d62248
Sohag Hasan <mailsohaghasan@gmail.com>
2022-02-05 20:48:42 +0600

    panda

---
+++ panda.js
@@ -0,0 +1,15 @@
+// // // problem 2 : pandaCost
+
+function pandaCost(singara, samocha, jilapi) {
+    var singaraResult = singara * 7;
+    var samochaResult = samocha * 10;
+    var jilapiResult = jilapi * 15;
+    var Result = console.log(singaraResult, samochaResult, jilapiResult);
+    return Result;
+}
+
+var singara = 5;
+var samocha = 5;
+var jilamp = 5;
+var totalPrice = pandaCost(singara, samocha, jilamp);
+console.log(totalPrice);
--- problem-1.js
+++ problem-1.js
@@ -9,49 +9,50 @@
 // var Vori = anaToVori(myana);
 // console.log(Vori);
 
-// // // // problem 2 : pandaCost
-// function pandaCost(singara, somocha, jilapi) {
-//     var singaraResult = singara * 7;
-//     var samochaResult = somocha * 10;
-//     var jilapiResult = jilapi * 15;
-//     var Result = console.log(singaraResult, samochaResult, jilapiResult);
-//     return Result;
-// }
+// // // problem 2 : pandaCost
 
-// var singara = 5;
-// var samocha = 5;
-// var jilamp = 5;
-// var totalPrice = pandaCost(singara, samocha, jilamp);
-// console.log(totalPrice);
+function pandaCost(singara, samocha, jilapi) {
+    var singaraResult = singara * 7;
+    var samochaResult = samocha * 10;
+    var jilapiResult = jilapi * 15;
+    var Result = console.log(singaraResult, samochaResult, jilapiResult);
+    return Result;
+}
 
-// // // problem 3 : picnicBudget
+var singara = 5;
+var samocha = 5;
+var jilamp = 5;
+var totalPrice = pandaCost(singara, samocha, jilamp);
+console.log(totalPrice);
+
+// // problem 3 : picnicBudget
 
 // picnicBudget
 
 
 
 // // // problem 4 : oddFriend
-var friends = ["sohada", "roni", "mamon", "kkki", "aaa"];
-console.log(friends[4]);
+// var friends = ["sohada", "roni", "mamon", "kkki", "aaa"];
+// console.log(friends[4]);
 
-function oddFriend(friends) {
+// function oddFriend(friends) {
 
-    for (let i = 0; i <= 4; i++) {
-        var newFriend;
-        newFriend = friends[i];
-        var stringLength = newFriend.length;
-        if (stringLength % 2 == 0) {
-            // It means it is Even
-        } else {
-            // Or it means it is Odd
-            break;
-        }
-    }
+//     for (let i = 0; i <= 4; i++) {
+//         var newFriend;
+//         newFriend = friends[i];
+//         var stringLength = newFriend.length;
+//         if (stringLength % 2 == 0) {
+//             // It means it is Even
+//         } else {
+//             // Or it means it is Odd
+//             break;
+//         }
+//     }
 
-    return newFriend;
-}
-let a = oddFriend(friends);
-console.log(a);
+//     return newFriend;
+// }
+// let a = oddFriend(friends);
+// console.log(a);
 
 // Difference -  let , var , const
 // const a = 'ssss';
