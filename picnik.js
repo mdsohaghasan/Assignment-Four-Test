06c9c4921258340c75d6b2e315d0132f3c540b6c
Sohag Hasan <mailsohaghasan@gmail.com>
2022-02-06 20:19:13 +0600

    Update picnik.js

--- picnik.js
+++ picnik.js
@@ -1,4 +1,45 @@
+// problem solving:3 picnic Budget
 
+function picnicBudget(totalPeople) {
+    const first100 = 5000;
+    const sec100 = 4000;
+    const third100 = 3000;
+    let totalCost1 = (first100 * totalPeople);
+    let totalCost2 = (first100 * totalPeople * sec100);
+    let totalCost3 = (first100 * totalPeople * sec100 * third100);
+    switch (totalPeople <= 0) {
+        case console.log("Please give the number from 0 onwards "):
+
+            break;
+        case (totalPeople <= 100):
+            console.log(totalCost1);
+            break;
+        case (totalPeople <= 200):
+            console.log(totalCost2);
+            break;
+        case (totalPeople <= 300):
+            console.log(totalCost3);
+            break;
+        default:
+            text = "Please give the number from 0 onwards ";
+
+    }
+
+    // if(totalPeople<=0){
+    //     console.log("Please give the number from 0 onwards ");
+    // }
+    // else if(totalPeople<=100){
+    //     console.log(totalCost1);
+    // }
+    // else if(totalPeople<=200){
+    //     console.log(totalCost2);
+    // }
+    // else if(totalPeople>200){
+    //     console.log(totalCost3);
+    // }
+}
+
+picnicBudget(155);
 
 
 
